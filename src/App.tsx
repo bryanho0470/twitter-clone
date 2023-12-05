bdc9e88eec568e5ced3d06464f1bf2c0dfb1ab68
bryanho0470 <bryanho0470@gmail.com>
2023-12-05 13:31:20 +0900

    initialize auth system
    enable auth
    and initialized on the firebase.ts file.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const router = createBrowserRouter([
   {
@@ -43,6 +44,7 @@ body {
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
+    await auth.authStateReady();
     setLoading(false);
   };
   useEffect(() => {
