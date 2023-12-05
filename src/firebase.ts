594232ff5804f5dee36d1958c010ab66b86b2300
bryanho0470 <bryanho0470@gmail.com>
2023-12-05 13:22:01 +0900

    Initialize firebase
    Create project on the firerbase PF
    install firebase 10.1.0
    Create firebase.tx file in src folder.

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8i5iBpvnRySWt0iWhPtSn7W0fjc_wwE4",
+  authDomain: "nwtter-cloned.firebaseapp.com",
+  projectId: "nwtter-cloned",
+  storageBucket: "nwtter-cloned.appspot.com",
+  messagingSenderId: "755848706978",
+  appId: "1:755848706978:web:44697c6aed99e0d58990fb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
